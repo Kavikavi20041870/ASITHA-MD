1a76a5db8e19de6e634d712d37cfb5838278734e
Kavikavi20041870 <kavishkamonday@gmail.com>
2024-10-26 17:11:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Tuh3mb6C#xHmMGVHZssoY9hOfa-fbsJzRpfjS-qJ6LxbEvt-1eVw",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=K3xiURqZ#Nf0VqwbPeXVEmuh4P8vK0AhAixlDLJygDBfe_4LRjsc",
 MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
